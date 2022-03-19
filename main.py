87810a60444c698a7e7bd6bfa0008f61b582d078
Donsky1 <falson2007@yandex.ru>
2022-03-19 13:31:47 +0300

    update project test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 from flask import Flask
 
-
+# получение api ответа с сайта
 def get_valutes_list():
     url = 'https://www.cbr-xml-daily.ru/daily_json.js'
     response = requests.get(url)
@@ -13,9 +13,10 @@ def get_valutes_list():
 
 app = Flask(__name__)
 
-
+# функция создания html таблицы
 def create_html(valutes):
-    text = '<h1>Курс валют</h1>'
+    text = '<a href="https://neural-university.ru/"><img src="https://static.tildacdn.com/tild6239-3638-4630-b161-343433313934/UII8_3.png" alt="Пример"></a>'
+    text += '<h1>Курс валют</h1>'
     text += '<table>'
     text += '<tr>'
     for _ in valutes[0]:
